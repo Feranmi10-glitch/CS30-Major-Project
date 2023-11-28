1a9633629bedc633ea692a8b76625bce4de2bcb5
Feranmi10-glitch <146806749+Feranmi10-glitch@users.noreply.github.com>
2023-11-28 12:17:01 -0600

    Starting to build the grid

--- sketch.js
+++ sketch.js
@@ -4,64 +4,27 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-// let cellSize;
-// const GRID_SIZE1 = 3;
-// const GRID_SIZE2 = 1;
-// const GRID_SIZE3 = 5;
-// const CELL_SPACE1 = 100;
-// const CELL_SPACE2 = 800;
-// const CELL_SPACE3 = 250;
-// const CELL_SPACE4 = 150;
-class Cell{
-  constructor(gridX, gridY){
-    this.gridX = gridX;
-    this.gridY = gridY;
+let gridArray = [];
+
+class Grid{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.gridA = 3;
   }
 }
 
-
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // if (height > width) {
-  //   cellSize = width/5;
-  // }
-  // else {
-  //   cellSize = height/5;
-  // }
+  let theGrid = new Grid(width/2, height/2);
+  gridArray.push(theGrid);
   
 }
 
 
 function draw() {
-  background("black");
-  // displayGrid();
+  background("white");
+  
 }
 
-// function displayGrid(){
-//   for(let y = 0; y < GRID_SIZE1; y++){
-//     for(let x = 0;x < GRID_SIZE1; x++){
-//       rect(x*cellSize+CELL_SPACE1, y*cellSize+CELL_SPACE1, cellSize, cellSize);
-//     }
-//     for(let y = 0; y < GRID_SIZE2; y++){
-//       for(let x = 0;x < GRID_SIZE3; x++){
-//         rect(x*cellSize+CELL_SPACE2, y*cellSize+CELL_SPACE3, cellSize, cellSize);
-//       }
-//       for(let y = 0; y < GRID_SIZE2; y++){
-//         for(let x = 0;x < GRID_SIZE3; x++){
-//           rect(x*cellSize+height/2, y*cellSize+(height-CELL_SPACE4), cellSize, cellSize);
-//         }
-//       }
-//     }
-//   }
-// }
 
-// function generateEmptyGrid(cols, rows) {
-//   let randomArray = [];
-//   for (let y = 0; y < cols; y++) {
-//     randomArray.push([]);
-//     for (let x = 0; x < rows; x++) {
-//       randomArray[y].push(0);
-//     }
-//   }
-//   return randomArray;
-// }
