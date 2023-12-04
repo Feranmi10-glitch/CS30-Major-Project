6cba0ce384c8663988b94ff168c9f834865d299c
Feranmi10-glitch <146806749+Feranmi10-glitch@users.noreply.github.com>
2023-12-04 12:17:01 -0600

    Created one grid

--- sketch.js
+++ sketch.js
@@ -5,8 +5,11 @@
 // - describe what you did to take this project "above and beyond"
 
 let gridArray = [];
-let cols;
-let rows;
+let colsA = 3;
+let rowsA = 3;
+let w;
+let colsB = 1;
+let rowsB = 5;
 
 class GridA{
   constructor(x, y){
@@ -16,20 +19,49 @@ class GridA{
   }
 
   display(){
-
+    rect(this.x*w, this.y*w, w, w);
   }
 
 }
 
+class GridB{
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    
+  }
+
+  display(){
+    rect(this.x*w, this.y*w, w, w);
+  }
+
+}
+
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
- 
+  if(height > width){
+    w = Math.floor(width/colsA);
+  }
+  else{
+    w = Math.floor(height/rowsA);
+  }
+  
+  for(let y = 0; y < colsA; y++ ){
+    for(let x = 0;  x< rowsA; x++){
+      let cell = new GridA(x,y);
+      gridArray.push(cell);
+    }
+  }
   
 }
 
 
 function draw() {
   background("white");
+  for(let x= 0; x<gridArray.length;x++){
+    gridArray[x].display();
+  }
   
 }
 
